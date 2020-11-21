484d71cf7b0573156221d38ec9ae6b64d7cd03c0
jpb06 <jpb.06@outlook.com>
2020-11-21 15:56:59 +0100

    constraints typo

--- back/src/constraints/index.ts
+++ back/src/constraints/index.ts
@@ -1,5 +1,5 @@
 export type ConstraintSource = "body" | "params" | "query";
 
 export * from "./email.constraint";
-export * from "./mongoId.constraints";
+export * from "./mongoId.constraint";
 export * from "./string.constraint";
