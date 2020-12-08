bf3966dad19dcea49d91e734c7b10a4f4f5ef0bc
jpb06 <jpb.06@outlook.com>
2020-12-08 19:18:39 +0100

    Adding invitations to the dbase

--- back/src/types/database.interface.ts
+++ back/src/types/database.interface.ts
@@ -1,8 +1,9 @@
-import { Team, User } from "../../../front/src/stack-shared-code/types";
+import { Invitation, Team, User } from "../../../front/src/stack-shared-code/types";
 import { AppKey } from "./app.key.interface";
 
 export default interface Database {
   users: Array<User>;
   teams: Array<Team>;
   appKeys: Array<AppKey>;
+  invitations: Array<Invitation>;
 }
