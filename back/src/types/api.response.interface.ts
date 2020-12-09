a45841ab91a93eb801555b744cabb2410ba34b97
jpb06 <jpb.06@outlook.com>
2020-12-09 17:55:11 +0100

    Adding answerfrom helper

--- back/src/types/api.response.interface.ts
+++ back/src/types/api.response.interface.ts
@@ -1,7 +1,13 @@
 import { Response } from "express";
 
+export interface AnswerData {
+  code: number;
+  text: any;
+}
+
 export interface ApiResponse extends Response {
   populate: (data: any) => Response;
   answer: (status: number, data: any) => Response;
+  answerFrom: (data: AnswerData) => Response;
   terminate: (status: number, message: string) => void;
 }
