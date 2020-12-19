1499c000a5d8f2e0676f0d3bcf13ec4514d57d21
jpb06 <jpb.06@outlook.com>
2020-12-19 12:50:55 +0100

    Creating a dedicated response for logged in routes

--- back/src/types/express-response/logged.user.response.interface.ts
+++ back/src/types/express-response/logged.user.response.interface.ts
@@ -1,7 +1,10 @@
+import { User } from "../../../../front/src/stack-shared-code/types";
 import { ApiResponse } from "./api.response.interface";
 
 export interface LoggedUserResponse extends ApiResponse {
   locals: {
+    routeLogs: Array<string>;
     userId: string;
+    loggedUser: User;
   };
 }
