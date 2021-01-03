45a191d99412cc3eed4fbf4694f4e99c6b206181
jpb06 <jpb.06@outlook.com>
2021-01-03 21:10:23 +0100

    Ignoring routes mapping

--- back/jest.config.js
+++ back/jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  roots: [
+    "<rootDir>/src"
+  ],
   testMatch: ["**/__tests__/**/*.+(ts)", "**/?(*.)+(spec|test).+(ts)"],
   transform: {
     "^.+\\.(ts)$": "ts-jest",
@@ -10,6 +13,10 @@ module.exports = {
     "/node_modules/",
     "/src/dal/mockdb/",
     "/src/constraints/",
+    "/src/routes/main.router.ts",
+    "/src/routes/anonymous.routes.ts",
+    "/src/routes/logged.in.routes.ts",
+    "/src/tests-related/",
     "/src/my-day-api.ts",
     "index.ts",
     ".d.ts"
